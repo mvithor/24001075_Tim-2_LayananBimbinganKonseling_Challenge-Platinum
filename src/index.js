c9f6936763731ea92b4bc988be3a585fda84f701
naufalalhannan <naufalalhannan@gmail.com>
2024-06-24 20:55:02 +0700

    Add Redux PersistGate

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './store/Store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store/Store';
 import Spinner from './views/spinner/Spinner';
 // import './_mockApis';
 import './utils/i18n';
@@ -11,10 +12,12 @@ import './utils/i18n';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+     <PersistGate loading={null} persistor={persistor}>
     <Suspense fallback={<Spinner />}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Suspense>
+    </PersistGate>
   </Provider>,
 );
