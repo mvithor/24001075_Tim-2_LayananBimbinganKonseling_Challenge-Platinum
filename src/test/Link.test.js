9a7f1321109615dc700afdfd675af2796c5b11d8
Ray Syadera <syaderay1@gmail.com>
2024-06-27 04:11:47 +0700

    tinggal package jsonnya aja

---
+++ src/test/Link.test.js
@@ -0,0 +1,26 @@
+import renderer from 'react-test-renderer';
+import Link from '../components/test/Link';
+
+it('changes the class when hovered', () => {
+  const component = renderer.create(
+    <Link page="http://www.facebook.com">Percobaan</Link>,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.onMouseEnter();
+  });
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.onMouseLeave();
+  });
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
