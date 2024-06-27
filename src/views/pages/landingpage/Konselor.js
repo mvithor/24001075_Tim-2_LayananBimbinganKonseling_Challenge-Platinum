df838a82899d92944b4c2204f19ec139a193d44c
naufalalhannan <naufalalhannan@gmail.com>
2024-06-27 08:23:17 +0700

    ya

--- src/views/pages/landingpage/Konselor.js
+++ src/views/pages/landingpage/Konselor.js
@@ -4,7 +4,7 @@ import PageContainer from 'src/components/container/PageContainer';
 import Footer from '../../../components/landingpage/Footer';
 import LpHeader from '../../../components/landingpage/Header';
 import ImgCarousel from '../../../components/landingpage/home/carousel/Carousel';
-import DetailKonselor from '../../../components/landingpage/konselor/Konselor'
+import DetailKonselor from '../../../components/landingpage/Konselor/Konselor';
 
 
 const KonselorPage = () => {
