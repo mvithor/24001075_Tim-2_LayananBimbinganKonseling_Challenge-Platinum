a8b81b14a918b3da1a8a53aac1104e8d14950a32
naufalalhannan <naufalalhannan@gmail.com>
2024-06-27 08:25:06 +0700

    last

--- src/views/pages/landingpage/LiterasiDetail.js
+++ src/views/pages/landingpage/LiterasiDetail.js
@@ -3,7 +3,7 @@ import PageContainer from 'src/components/container/PageContainer';
 
 import Footer from '../../../components/landingpage/Footer';
 import LpHeader from 'src/components/landingpage/Header';
-import LiterasiDetail from 'src/components/landingpage/literasi/LiterasiDetail';
+import LiterasiDetail from 'src/components/landingpage/Literasi/LiterasiDetail';
 
 const LiterasiDetailViews = () => {
     return (
