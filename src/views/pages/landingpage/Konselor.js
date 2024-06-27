9a4b49236db846e15cdc7beaaa4a6685a1b3eb1d
naufalalhannan <naufalalhannan@gmail.com>
2024-06-27 07:41:58 +0700

    fix component konselor

--- src/views/pages/landingpage/Konselor.js
+++ src/views/pages/landingpage/Konselor.js
@@ -4,14 +4,14 @@ import PageContainer from 'src/components/container/PageContainer';
 import Footer from '../../../components/landingpage/Footer';
 import LpHeader from '../../../components/landingpage/Header';
 import ImgCarousel from '../../../components/landingpage/home/carousel/Carousel';
-import DetailKonselor from '../../../components/landingpage/konselor/Konselor'; 
+import Konselor from '../../../components/landingpage/konselor/Konselor'; 
 
 const LandingPage = () => {
   return (
     <PageContainer title="Konselor" description="testimoni konselor">
       <LpHeader />
       <ImgCarousel />
-      <DetailKonselor />     
+      <Konselor/>  
       <Footer />
     </PageContainer>
   );
